6f6e4d978810dd2a2f19a95f63067695743f311f
Linker <447119040@qq.com>
2017-02-27 11:40:53 +0800

    Add files via upload

---
+++ main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  ch-05
+//
+//  Created by s20151104698 on 2017/2/27.
+//  Copyright © 2017年 s20151104698. All rights reserved.
+//
+
+import Foundation
+
+var num=0
+for var i in 1...100
+{
+    num=num+i;
+}
+print(num)
